f55000a8cfe253b88ab5faf5d751a7ca6f9c079a
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-05 13:23:30 +0600

    first commit

---
+++ module1/src/1.7.ts
@@ -0,0 +1,60 @@
+{
+// spread operator and destructure , rest operator
+
+const bros1: string[]= ["a", "b", "c"]
+
+const bros2: string[]=["d", "e", "f"]
+
+bros1.push(...bros2)
+
+
+const mentor1 = {
+    typeScript: "Mezba bhai",
+    Next: "Parvez"
+
+}
+const mentor2= {
+    DBMS: "Tanmoy",
+    Prizma: "Jhankar"
+}
+
+const mentorList = {
+...mentor1,
+...mentor2
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
