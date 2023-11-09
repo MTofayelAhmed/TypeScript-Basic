744658dd2317b1788b9c2c2d4ee7b0117b9d4680
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-10 00:47:37 +0600

    done with type assertion

--- module2/2.1.ts
+++ module2/2.1.ts
@@ -6,6 +6,25 @@ anything = "next level web development";
 anything = 222;
 (anything as number )
 
+//  another example 
+
+const kgToGm = (value : number | string) : number | string | undefined=>{
+    if(typeof value ==="string" ){
+        const convertedValue = parseFloat(value)*1000;
+        return `the converted value  is ${convertedValue}`
+    }
+
+    if (typeof value === "number" ){
+        return value*1000;
+
+    }
+}
+
+const result1 = kgToGm(1000) as number;
+
+const result2 = kgToGm("1000")as string;
+
+
 
 
 
