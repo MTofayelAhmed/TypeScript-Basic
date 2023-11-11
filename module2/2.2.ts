e8fabf833ba2cdce7938878cfa464a96ad5c355a
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-11 12:41:17 +0600

    done with commit

--- module2/2.2.ts
+++ module2/2.2.ts
@@ -10,27 +10,61 @@ type user1 = {
     age: number;
 }
 
-const user: user1 = {
-name: "John",
-age: 12
-}
-
-
-
-
+type rollNumber = number;
+type userWithRole = user1 & {role: string}
+const user: userWithRole = {
+    name: "John",
+    age: 12,
+    role: "admin"
+    }
 // interface 
+// interface start with {} , so we can not declare primitive types through this 
+// interface user2{
+//     name: string;
+//     age: number;
+// }
+// interface userWithRole2 extends user2 {
+//     role: string;
+// }
+// const user2 : userWithRole2 = {
+// name: "money",
+// age: 12,
+// role: "manager"
 
-interface user2{
+// }
+
+interface user2 {
     name: string;
     age: number;
-}
-
-const user2 : user2 = {
-name: "money",
-age: 12
-
 
 }
+
+// const user2 = {
+//     name: "tofayel",
+//     age: 34
+
+// }
+
+interface userWithRole3 extends user2 {
+    role: string;
+}
+
+const user2 : userWithRole3 = {
+   name: "abandon",
+   age: 35,
+   role: "director" 
+}
+
+
+type Roll1 = number[] ;
+const rollNumber : Roll1 = [1, 2, 3, 4]
+
+interface Roll2 {
+    [index: number]: number;
+}
+
+
+
 
 
 
