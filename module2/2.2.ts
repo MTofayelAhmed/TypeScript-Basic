b3fe0369fc95935275a867cf556dc9e58b5fe784
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-11 10:17:15 +0600

    type interface

---
+++ module2/2.2.ts
@@ -0,0 +1,38 @@
+
+
+{
+
+    // type alias 
+    // type alias can also be used for primitive type
+
+type user1 = {
+    name: string ;
+    age: number;
+}
+
+const user: user1 = {
+name: "John",
+age: 12
+}
+
+
+
+
+// interface 
+
+interface user2{
+    name: string;
+    age: number;
+}
+
+const user2 : user2 = {
+name: "money",
+age: 12
+
+
+}
+
+
+
+
+}
