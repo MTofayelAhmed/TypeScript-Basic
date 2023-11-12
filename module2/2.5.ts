62f1697b3b269ed584ff8a27aeb26c333b63f0c1
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-12 14:39:52 +0600

    done with generic

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -1,24 +1,39 @@
 {
-    // function with generics 
+  // function with generics
+
+  const createArray = (param: string): string[] => {
+    return [param];
+  };
+
+  const res1 = createArray("Bangladesh");
+
+  const createArrayWithGenerics = <T>(param: T): T[] => {
+    return [param];
+  };
+
+  const resGeneric = createArrayWithGenerics<boolean>(true);
+  type User = { id: number; name: string };
+
+  const resGenericWithObj = createArrayWithGenerics<User>({
+    id: 1234,
+    name: "Bangladesh",
+  });
+
+  console.log(resGenericWithObj);
+  const createArrayWithTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+
+  const addCourseToStudent = <T>(student: T) => {
+    const course = "Next level web Development";
+
+    return {
+      ...student,
+      course,
+    };
 
 
+  };
 
-const createArray = (param: string): string[]=> {
-return [param]
-}
-
-const res1 = createArray("Bangladesh")
-
-
-const createArrayWithGenerics = <T> (param: T): T[]=> {
-    return [param]
-}
-
-const resGeneric = createArrayWithGenerics<boolean>(true)
-type User = {id: number; name: string}
-
-
-const resGenericWithObj = createArrayWithGenerics<User>({id: 1234, name: "Bangladesh" })
-
-console.log(resGenericWithObj)
+  const student1 = addCourseToStudent<{id: number; name: string}>({id: 123456, name: "Bangladesh"})
 }
