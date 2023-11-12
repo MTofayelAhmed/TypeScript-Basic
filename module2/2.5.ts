812185ee516c4ead54b593b8ffda359d469078f5
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-12 14:07:05 +0600

    done with function generics

--- module2/2.5.ts
+++ module2/2.5.ts
@@ -3,10 +3,22 @@
 
 
 
-const createArray = (param: string)=> {
+const createArray = (param: string): string[]=> {
 return [param]
 }
 
+const res1 = createArray("Bangladesh")
 
 
+const createArrayWithGenerics = <T> (param: T): T[]=> {
+    return [param]
+}
+
+const resGeneric = createArrayWithGenerics<boolean>(true)
+type User = {id: number; name: string}
+
+
+const resGenericWithObj = createArrayWithGenerics<User>({id: 1234, name: "Bangladesh" })
+
+console.log(resGenericWithObj)
 }
