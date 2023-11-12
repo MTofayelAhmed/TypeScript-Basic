68b608c03ae9f938f455bd3cacf8268aeae93c20
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-12 16:21:12 +0600

    done with constrin using keyof

--- module2/2.7.ts
+++ module2/2.7.ts
@@ -2,23 +2,32 @@
 
     // generic constraints with keyof operator
 
-type vehicle = {
-    bike : string;
-    car: string;
-    ship: string;
-}
+// type vehicle = {
+//     bike : string;
+//     car: string;
+//     ship: string;
+// }
 
-type owner = "bike" | "car" | "ship"
-
-
+// type owner = "bike" | "car" | "ship"
 
 
 
+// type owner2 = keyof vehicle
+
+// const person1 : owner = ""
+
+const user = {
+    name: "JOHN",
+    age: 20,
+    address: "ctg"}
 
 
+    const getObjectKey= <X, Y extends keyof X>(obj: X, key: Y)=> {
+        return obj[key]
+    }
 
 
-
+    const result2 = getObjectKey(user, "name")
 
 
 
