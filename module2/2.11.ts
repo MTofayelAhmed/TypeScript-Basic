2286794081636adf800195270b3e98355a6781b3
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-13 14:10:18 +0600

    done with utility

--- module2/2.11.ts
+++ module2/2.11.ts
@@ -13,6 +13,36 @@ type Person = {
 type Name = Pick<Person, "name" | "age">
 
 
+// Omit 
+
+type contactInfo = Omit<Person, "name"| "age">
+
+
+
+// required 
+type mustInfo = Required<Person>
+// partial
+type personPartial =  Partial<Person>
+
+// readOnly
+
+type PersonReadonly = Readonly<Person>
+
+const person1 : PersonReadonly = {
+    name: "John",
+    age: 12,
+    contactNo: "01684163259",
+    email: "john@gmail.com"
+}
+
+
+
+
+
+
+
+
+
 
 
 }
