8493493e5abaa6a72e30608ffb93d2eec65ce9c7
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-14 19:01:22 +0600

    problem solving

---
+++ module1/conceptual-session/1.1.ts
@@ -0,0 +1,23 @@
+
+  type User = {
+    id: number;
+    name: string;
+  };
+
+  const users: Array<User> = [
+    {
+      id: 1,
+      name: "Tofayel",
+    },
+
+    {
+      id: 2,
+      name: "Selim",
+    }
+  ];
+
+
+
+function  getPropertyFromArray (){
+    
+}
