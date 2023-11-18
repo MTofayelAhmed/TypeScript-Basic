6ae9b1d7d9db8622db93abdfac9f88c5423f0232
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-18 18:13:05 +0600

    done with inheritence concepts

--- module3/3.2.ts
+++ module3/3.2.ts
@@ -1,29 +1,65 @@
 {
-// inheritance 
+  // inheritance
 
-class Student {
-  name : string;
-  age : number;
-  address : string;
-    
-  constructor(name : string, age : number, address: string){
-    this.name = name ;
-    this.age = age ;
-    this.address = address;
+  class Parent {
+    name: string;
+    age: number;
+    address: string;
+
+    constructor(name: string, age: number, address: string) {
+      this.name = name;
+      this.age = age;
+      this.address = address;
+    }
+
+    getSleep(NumOfHour: number) {
+      console.log(`${this.name} slept for ${NumOfHour} hours.`);
+    }
+  }
+
+
+
+  class Student extends  Parent{
+  
+
+    constructor(name: string, age: number, address: string) {
+      super(name, age, address);
+    }
+
 
   }
 
-getSleep(NumOfHour: number){
+  const student1 = new Student("Tofayel", 20, "Dhaka")
+  
+
+
+
+
+
+  class Teacher extends Parent {
+   
+    designation: string;
+
+    constructor(
+      name: string,
+      age: number,
+      address: string,
+      designation: string
+    ) {
+        super(name, age, address)
     
-}
-
-}
-
+      this.designation = designation;
+    }
 
 
 
+    TakeClasses(NumOfClasses: number) {
+      console.log(`${this.name} takes  ${NumOfClasses} classes`);
+    }
+  }
 
 
+  const teacher = new Teacher ("porimol", 40, "Dhaka", "professor")
 
-
+  teacher.TakeClasses(20)
 }
