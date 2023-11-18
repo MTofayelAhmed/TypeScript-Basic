80d55ba43a3df642d5d9c9884d21934c77b9e3ae
MTofayelAhmed <tofayelahmed324@gmail.com>
2023-11-18 20:39:10 +0600

    done with type guard in use

--- module3/3.3.ts
+++ module3/3.3.ts
@@ -1,18 +1,52 @@
 {
-  // type of operator
+  // type 
 
+  type alphaNumeric = number | string
   const add = (
-    param1: number | string,
-    param2: number | string
-  ): number | string => {
+    param1: alphaNumeric,
+    param2: alphaNumeric,
+  ): alphaNumeric => {
     if (typeof param1 === "number" && typeof param2 === "number") {
       return param1 + param2;
     }
     else {
-        param1.toString() + param2.toString()
+     return   param1.toString() + param2.toString()
     }
   };
 
   const result1 = add("2", "3")
   console.log(result1)
+// in guard
+
+
+type NormalUser = {
+    name: string,
+}
+
+type AdminUser = {
+    name: string,
+    role: "admin"
+} 
+
+const getUser = (user: AdminUser | NormalUser)=>{
+    if ("role" in user){
+        console.log(`my name is ${user.name} and my role is ${user.role}`)
+    }
+    else {
+        console.log(`my name is ${user.name } `)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
